8c9b7f0ec43c0b5146c162781ee3a78a540fa07b
Tony <coolhzb@163.com>
2014-04-27 01:06:58 +0800

    add service and mysql jdbc driver...

---
+++ src/main/java/com/ado/java/odata/service/ODataService.java
@@ -0,0 +1,11 @@
+package com.ado.java.odata.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nankonami
+ * Date: 14-4-27
+ * Time: 上午1:05
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ODataService {
+}
---
+++ src/main/java/com/ado/java/odata/service/impl/ODataServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ado.java.odata.service.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nankonami
+ * Date: 14-4-27
+ * Time: 上午1:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class ODataServiceImpl {
+}
